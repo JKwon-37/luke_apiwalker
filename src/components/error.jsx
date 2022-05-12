47eb68bb596b91532e3f61eb2483c65f1aed8d15
Jonathan <jonathankwon@yahoo.com>
2022-05-12 11:55:47 -0700

    update conditionals for rendering, link error, add back button to go back one page

--- src/components/error.jsx
+++ src/components/error.jsx
@@ -1,10 +1,21 @@
 import React from 'react'
+import { useNavigate } from 'react-router-dom';
 
 const Error = () => {
+    const navigate = useNavigate();
+
+    const backButton = (e) => {
+        e.preventDefault();
+        navigate(-1);
+    }
+
   return (
     <div>
         <h1>ERROR 404</h1>
         <img src={'https://www.belloflostsouls.net/wp-content/uploads/2022/02/NotTheDroids.png'} alt={'404'}></img>
+        <form onSubmit={backButton}>
+            <button>Back</button>
+        </form>
     </div>
   )
 }
--- src/components/starwarsAPI.jsx
+++ src/components/starwarsAPI.jsx
@@ -1,10 +1,12 @@
 import React, {useEffect, useState} from "react";
 import axios from "axios";
 import { useParams } from "react-router-dom";
+import { useNavigate } from "react-router-dom";
 
 const StarwarsAPI = (props) => {
     const [obj, setObj] = useState(null);
     const {what, id} = useParams();
+    const navigate = useNavigate();
     console.log(JSON.stringify(obj));
 
     useEffect( () => {
@@ -12,23 +14,40 @@ const StarwarsAPI = (props) => {
             .then(res => {
                 // console.log(res.data);
                 setObj(res.data);
-            }).catch(err => console.log(err))
+            }).catch(err => {
+                console.log(err)
+                navigate("/error", {replace: true})
+            })
+    }, [what, id, navigate])
 
-    }, [what, id])
+    const backButton = (e) => {
+        e.preventDefault();
+        navigate(-1);
+    }
 
-    return (
-        <div>
-            {/* <h1>{JSON.stringify(obj)}</h1> */}
+    if(what === 'people'){
+        return (
+            <div>
+                {/* <h1>{JSON.stringify(obj)}</h1> */}
+                    {obj ?
+                        <>
+                        <h1>{what} is {obj.name}</h1>
+                        <p style={{fontWeight: 'bold'}}>Height: {obj.height}</p>
+                        <p style={{fontWeight: 'bold'}}>Mass: {obj.mass}</p>
+                        <p style={{fontWeight: 'bold'}}>Hair Color: {obj.hair_color}</p>
+                        <p style={{fontWeight: 'bold'}}>Skin Color: {obj.skin_color}</p>
+                        </> : 'Loading...' 
+                    }
+                    <form onSubmit={backButton}>
+                        <button>Back</button>
+                    </form>
+                </div>
+                )
+        }
+    else if (what === 'planets') {
+        return (
+            <div>
                 { obj ?
-                    <>
-                    <h1>{what} is {obj.name}</h1>
-                    <p style={{fontWeight: 'bold'}}>Height: {obj.height}</p>
-                    <p style={{fontWeight: 'bold'}}>Mass: {obj.mass}</p>
-                    <p style={{fontWeight: 'bold'}}>Hair Color: {obj.hair_color}</p>
-                    <p style={{fontWeight: 'bold'}}>Skin Color: {obj.skin_color}</p>
-                    </> : 'Loading...' 
-                }
-                {/* { obj ?
                     <>
                     <h1>{what} is {obj.name}</h1>
                     <p style={{fontWeight: 'bold'}}>Height: {obj.climate}</p>
@@ -36,10 +55,20 @@ const StarwarsAPI = (props) => {
                     <p style={{fontWeight: 'bold'}}>Hair Color: {obj.surface_water}</p>
                     <p style={{fontWeight: 'bold'}}>Skin Color: {obj.population}</p>
                     </> : 'Loading...'
-                } */}
+                }
+                
+                <form onSubmit={backButton}>
+                    <button>Back</button>
+                </form>
+            </div>
+        )
+    }
+
                     
-        </div>
-    )
+        
+    
+
 }
+
 
 export default StarwarsAPI;
